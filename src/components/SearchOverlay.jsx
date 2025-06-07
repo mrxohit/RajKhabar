e15ae2a93b7fe838bb8259d49bbe9cec1f1ce140
mrxohit <xohit.email@gmail.com>
2025-06-07 18:43:36 +0530

    Update SearchOverlay.jsx

--- src/components/SearchOverlay.jsx
+++ src/components/SearchOverlay.jsx
@@ -13,7 +13,7 @@ const SearchOverlay = ({ isOpen, onClose }) => {
         <input
           type="text"
           placeholder="Search..."
-          className="flex border  border-none  px-4 py-2 rounded-md outline-none"
+          className="flex border w-[60vw] border-none  px-4 py-2 rounded-md outline-none"
         /> 
         <Search className="w-5 h-5 text-gray-500 ml-2" />
         
