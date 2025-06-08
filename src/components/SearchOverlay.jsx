974e06f6c0f7c286e11b7ec7b43bc805ce523a20
Xohit <mrxohit.email@gmail.com>
2025-06-08 12:32:55 +0530

    Update

--- src/components/SearchOverlay.jsx
+++ src/components/SearchOverlay.jsx
@@ -4,22 +4,22 @@ import { Home, Search, Cross, XIcon } from "lucide-react";
 
 const SearchOverlay = ({ isOpen, onClose }) => {
   return (
-   <div
-  className={`fixed inset-0 bg-white z-50 p-4 shadow-md transition-transform duration-300 ${
-    isOpen ? "translate-y-0" : "-translate-y-full"
-  }`}
->
-  <div className="flex items-center w-full max-w-[700px] mx-auto gap-2">
-    <input
-      type="text"
-      placeholder="Search..."
-      className="flex-1 px-4 py-2 rounded-md border border-gray-300 text-sm outline-none"
-    />
-    <Search className="w-5 h-5 text-gray-500" />
-    <XIcon onClick={onClose} className="w-5 h-5 cursor-pointer" />
-  </div>
-</div>
+    <div
 
+      className={`fixed aa top-0 left-0 flex w-[100vw ]  justify-center items-center right-0 bg-white z-50 p-4 shadow-md transition-transform duration-300 ${
+        isOpen ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
+      <div className="flex exam  p-1 items-center w-[90vw] justify-between">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="flex px-4 py-2 w-[60vw] rounded-md outline-none"
+        />
+        <Search className="w-5 h-5 text-gray-500 ml-2" />
+        <XIcon onClick={onClose} className=" ml-2 mr-2 w-5 h-5" />
+      </div>
+    </div>
   );
 };
 
