a833ab6e6d8c0bc0836dd1f02527e8d8b7e79bec
mrxohit <xohit.email@gmail.com>
2025-06-08 06:31:08 +0530

    Update SearchOverlay.jsx

--- src/components/SearchOverlay.jsx
+++ src/components/SearchOverlay.jsx
@@ -13,7 +13,7 @@ const SearchOverlay = ({ isOpen, onClose }) => {
         <input
           type="text"
           placeholder="Search..."
-          className="  w-[60vw]  px-4 py-2 rounded-md outline-none"
+          className="  w-[50vw]  px-4 py-2 rounded-md outline-none"
         /> 
         <Search className="w-5 h-5 text-gray-500 ml-2" />
         
