ab099fd52efaf900d2117fb850f5ee84ab378ee9
mrxohit <xohit.email@gmail.com>
2025-06-08 06:29:52 +0530

    Update SearchOverlay.jsx

--- src/components/SearchOverlay.jsx
+++ src/components/SearchOverlay.jsx
@@ -9,11 +9,11 @@ const SearchOverlay = ({ isOpen, onClose }) => {
         isOpen ? "translate-y-0" : "-translate-y-full"
       }`}
     >
-      <div className="flex w-[80vw] exam p-1    items-center justify-between">
+      <div className=" w-[80vw] exam p-1 justify-between">
         <input
           type="text"
           placeholder="Search..."
-          className="flex border w-[60vw] border-none  px-4 py-2 rounded-md outline-none"
+          className="  w-[60vw]  px-4 py-2 rounded-md outline-none"
         /> 
         <Search className="w-5 h-5 text-gray-500 ml-2" />
         
